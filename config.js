f158a57117924cc7dc03892d804bac72272d19e6
Nathaniel1531 <83643619+Nathaniel1531@users.noreply.github.com>
2022-05-25 00:58:26 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "on your birthday,     ",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "i thought of giving you the cutest gift in the world.",  // 同上...
+        "but then i realized that is not possible",
+        "because you yourself are the cutest gift in the world.",
+        "Have a loveliest happy birthday",
+        "to my loveliest girlfriend",
+        "Liza~~",
+    ],
+    
+    desc: {
+        turn_on: "Turn On Lights",
+        play: "Play Music",
+        bannar_coming: "Let's Decorate",
+        balloons_flying: "Fly With Balloons",
+        cake_fadein: "Most Delicious Cake Ever",
+        light_candle: "Light Candle",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
